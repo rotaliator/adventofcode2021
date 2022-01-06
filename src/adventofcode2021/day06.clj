df2e670d406d6a0bf3a1a0f8841639a25797d54a
rotaliator <rotaliator@gmail.com>
2022-01-06 15:12:25 +0100

    day06

--- src/adventofcode2021/day06.clj
+++ src/adventofcode2021/day06.clj
@@ -2,29 +2,25 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(def input (->> "day06.txt" io/resource slurp
-                (#(str/split % #","))
-                (map str/trim)
-                (mapv #(Integer/parseInt %))
-                (frequencies)))
-
-(def input' (->> (str/split "3,4,3,1,2" #",")
-                 (mapv #(Integer/parseInt %))
-                 (frequencies)))
+(def input (merge
+            (zipmap (range 9) (repeat 0))
+            (->> "day06.txt" io/resource slurp
+                       (#(str/split % #","))
+                       (map str/trim)
+                       (mapv #(Long/parseLong %))
+                       (frequencies))))
 
 (defn new-generation [old-generation]
   (reduce-kv (fn [m k v]
                (cond
-                 (< 0 k)
-                 (-> m
-                     (update (dec k) #((fnil + 0) % v))
-                     (update k #((fnil - 0) % v)))
+                 (< 0 k) (-> m
+                             (update (dec k) + v)
+                             (update k - v))
 
-                 :else
-                 (-> m
-                     (update k #((fnil - 0) % v))
-                     (update 8 #((fnil + 0) % v))
-                     (update 6 #((fnil + 0) % v))))) old-generation old-generation))
+                 :else (-> m
+                           (update k - v)
+                           (update 8 + v)
+                           (update 6 + v)))) old-generation old-generation))
 
 (defn state-after [input day]
   (last (take (inc day) (iterate new-generation input))))
@@ -37,5 +33,4 @@
   ;;part 2
   (reduce + (vals (state-after input 256)))
   ;; => 1644874076764
-
-  )
+)
